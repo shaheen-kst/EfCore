c7b06788b7e6d77127f37af45c37be2d50ffeac3
Shaheen-kst <shaheenelt@gmail.com>
2020-05-01 16:48:11 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
